8152a40ef462441f4cfdbd279cffb043e670982e
pbrose82 <34488965+pbrose82@users.noreply.github.com>
2025-04-09 07:51:47 -0400

    Update AppLayout.js

--- src/components/AppLayout.js
+++ src/components/AppLayout.js
@@ -34,19 +34,17 @@ function AppLayout({ children }) {
   return (
     <div className="app-container">
       {/* Left Navigation - Alchemized version */}
-      <div className="c-sidenav o-flex--column"
-           className={isExpanded ? 'c-sidenav--wide' : 'c-sidenav--mini'}
+      <div className={`c-sidenav o-flex--column ${isExpanded ? 'c-sidenav--wide' : 'c-sidenav--mini'}`}
            data-qa="appSideNav">
         <div className={isExpanded ? 'o-flex--wide' : 'u-width-64'}>
-          <div className="js-sidebar-header"
-               className={isExpanded ? 'u-margin-horizontal-l' : 'u-margin-horizontal-m'}>
+          <div className={`js-sidebar-header ${isExpanded ? 'u-margin-horizontal-l' : 'u-margin-horizontal-m'}`}>
             
             {/* Header with toggle button */}
             <div className="u-margin-bottom-s u-margin-top-xs o-flex-flag">
               <button className="c-button-icon-white72 o-flex o-flex--center o-flex--middle u-margin-right-xxs"
                       type="button"
                       title={isExpanded ? "Collapse sidebar" : "Expand sidebar"}
-                      onClick={onToggleSideNavSizeClick()}
+                      onClick={onToggleSideNavSizeClick}
                       data-qa="toggleSideNavSize">
                 {isExpanded ? (
                   <i className="fas fa-chevron-left u-color-white-opacity-72"></i>
@@ -63,8 +61,7 @@ function AppLayout({ children }) {
             </div>
 
             {/* Search box */}
-            <button className="c-btn-like-input c-btn-like-input__search o-flex--start u-margin-top-xxs u-padding-none u-line-height-0x"
-                    className={isExpanded ? 'u-1/1 u-margin-right-l' : 'u-width-32 u-min-width-32'}
+            <button className={`c-btn-like-input c-btn-like-input__search o-flex--start u-margin-top-xxs u-padding-none u-line-height-0x ${isExpanded ? 'u-1/1 u-margin-right-l' : 'u-width-32 u-min-width-32'}`}
                     title={isExpanded ? '' : 'Search'}
                     onClick={() => {}} 
                     data-qa="showQuickSearch">
@@ -83,8 +80,7 @@ function AppLayout({ children }) {
                       onClick={() => {}}
                       data-qa="newProcessAndView"
                       title={isExpanded ? '' : 'New'}
-                      className="white-mat-button-base u-margin-top-m u-margin-bottom-l u-color-blue-gray-800 u-line-height-0x"
-                      className={isExpanded ? 'u-margin-right-s' : 'u-width-32 u-min-width-32 u-padding-horizontal-ms'}>
+                      className={`white-mat-button-base u-margin-top-m u-margin-bottom-l u-color-blue-gray-800 u-line-height-0x ${isExpanded ? 'u-margin-right-s' : 'u-width-32 u-min-width-32 u-padding-horizontal-ms'}`}>
                 <div className="o-flex u-height-100 o-flex--middle">
                   <i className="fas fa-plus"></i>
                   {isExpanded && (
@@ -99,8 +95,7 @@ function AppLayout({ children }) {
           <div className="u-overflow-auto js-views-and-assignments u-1/1">
             <div className={isExpanded ? '' : 'c-nav-mini-menu--height u-overflow-y-auto u-overflow-x-hidden'}>
               <Link to="/" 
-                    className="c-sidenav__my-alchemy t-5 o-flex o-flex--middle u-padding-top-xs u-1/1 u-padding-horizontal-l"
-                    className={location.pathname === '/' ? 'c-selectable-item__dark--active' : ''}
+                    className={`c-sidenav__my-alchemy t-5 o-flex o-flex--middle u-padding-top-xs u-1/1 u-padding-horizontal-l ${location.pathname === '/' ? 'c-selectable-item__dark--active' : ''}`}
                     title={isExpanded ? null : 'Dashboard'}>
                 <div className="c-icon--medium u-background-transparent u-margin-right-xxs u-padding-left-none">
                   <i className="fas fa-th-large u-color-white-opacity-72"></i>
@@ -120,8 +115,7 @@ function AppLayout({ children }) {
               {/* Only show current tenant in nav if accessing directly via URL */}
               {tenantId && (
                 <Link to={`/${tenantId}`}
-                      className="c-sidenav__my-alchemy t-5 o-flex o-flex--middle u-margin-bottom-m u-padding-top-xs u-1/1 u-padding-horizontal-l"
-                      className={location.pathname === `/${tenantId}` ? 'c-selectable-item__dark--active' : ''}
+                      className={`c-sidenav__my-alchemy t-5 o-flex o-flex--middle u-margin-bottom-m u-padding-top-xs u-1/1 u-padding-horizontal-l ${location.pathname === `/${tenantId}` ? 'c-selectable-item__dark--active' : ''}`}
                       title={isExpanded ? null : `${tenantId} Calendar`}>
                   <div className="c-icon--medium u-background-transparent u-margin-right-xxs u-padding-left-none">
                     <i className="fas fa-calendar u-color-white-opacity-72"></i>
@@ -166,8 +160,7 @@ function AppLayout({ children }) {
         </div>
 
         {/* Footer copyright */}
-        <div className="c-sidenav__copy u-font-size-12 u-padding-vertical-s u-text-align-center"
-             className={!isExpanded ? 'u-height-32' : ''}>
+        <div className={`c-sidenav__copy u-font-size-12 u-padding-vertical-s u-text-align-center ${!isExpanded ? 'u-height-32' : ''}`}>
           <span>{isExpanded ? '© Alchemy Cloud, Inc. All Rights Reserved.' : '© Alchemy'}</span>
         </div>
       </div>
