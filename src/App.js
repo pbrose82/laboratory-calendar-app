39b4e1477f72ec388b1ff53cf812b51488f45842
pbrose82 <34488965+pbrose82@users.noreply.github.com>
2025-04-18 16:08:43 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import EquipmentList from './components/EquipmentList';
 import TechnicianSchedule from './components/TechnicianSchedule';
 import GanttChart from './components/GanttChart';
 import Analytics from './components/Analytics';
+import CapacityPlanning from './components/CapacityPlanning'; // Import the new component
 import AppLayout from './components/AppLayout';
 import 'antd/dist/reset.css'
 import './App.css';
@@ -38,12 +39,15 @@ function App() {
           } />
           <Route path="/:tenantId" element={<TenantCalendar />} />
           
-          {/* New Resource Planning Routes */}
+          {/* Resource Planning Routes */}
           <Route path="/:tenantId/resource-dashboard" element={<ResourceDashboard />} />
           <Route path="/:tenantId/equipment-list" element={<EquipmentList />} />
           <Route path="/:tenantId/technician-schedule" element={<TechnicianSchedule />} />
           <Route path="/:tenantId/gantt-chart" element={<GanttChart />} />
           <Route path="/:tenantId/analytics" element={<Analytics />} />
+          
+          {/* New Capacity Planning Route */}
+          <Route path="/:tenantId/capacity-planning" element={<CapacityPlanning />} />
         </Routes>
       </AppLayout>
     </Router>
